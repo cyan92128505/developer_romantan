b39854a5975c3c2249cb8fbfbbd80a0c7f68f8d7
cyan92128505 <cyan92128505@gmail.com>
2020-01-31 10:07:42 +0800

    Upgrade packages

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/aomashinku/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aomashinku/Dev/aoma/developer_romantan"
+export "FLUTTER_TARGET=/Users/aomashinku/Dev/aoma/developer_romantan/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/aomashinku/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
