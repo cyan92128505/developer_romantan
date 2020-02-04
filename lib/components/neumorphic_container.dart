5b8fa3da91821425ff8c7ce27a9556d5a5e58aca
cyan92128505 <cyan92128505@gmail.com>
2020-02-04 10:22:12 +0800

    Modify - refactoring neumorphic_container

--- lib/components/neumorphic_container.dart
+++ lib/components/neumorphic_container.dart
@@ -1,133 +1,185 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-enum CurveType { concave, convex, none }
+enum CurveType {
+  concave,
+  convex,
+  none,
+}
 
 class NeumorphicContainer extends StatefulWidget {
   final NeumorOption option;
-  final StreamController<String> streamController =
-      StreamController.broadcast();
 
   NeumorphicContainer(this.option);
 
   @override
   _NeumorphicContainerState createState() =>
-      _NeumorphicContainerState(this.streamController);
-
-  dispose() {
-    streamController.close();
-  }
-
-  toggle(bool status) {
-    this.option.emboss = status;
-    this.streamController.sink.add('v');
-  }
-
-  toggleOn() {
-    this.toggle(true);
-  }
-
-  toggleOff() {
-    this.toggle(false);
-  }
-
-  toggleTap() {
-    toggle(!this.option.emboss);
-  }
+      _NeumorphicContainerState(NeumorConfig.buildByNeumorOption(this.option));
 }
 
 class _NeumorphicContainerState extends State<NeumorphicContainer> {
-  final StreamController<String> streamController;
+  final NeumorConfig config;
 
-  _NeumorphicContainerState(this.streamController) : super() {
-    this.streamController.stream.listen((_) {
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    this.widget.dispose();
-    super.dispose();
-  }
+  _NeumorphicContainerState(this.config);
 
   @override
   Widget build(BuildContext context) {
-    double heightValue =
-        this.widget.option.height == null ? null : this.widget.option.height;
-    double widthValue =
-        this.widget.option.width == null ? null : this.widget.option.width;
-    int depthValue =
-        this.widget.option.depth == null ? 20 : this.widget.option.depth;
-    Color colorValue = this.widget.option.color == null
-        ? Color(0xFFf0f0f0)
-        : this.widget.option.color;
-    Color parentColorValue = this.widget.option.parentColor == null
-        ? colorValue
-        : this.widget.option.parentColor;
-    Color surfaceColorValue = this.widget.option.surfaceColor == null
-        ? colorValue
-        : this.widget.option.surfaceColor;
-    double spreadValue =
-        this.widget.option.spread == null ? 6 : this.widget.option.spread;
-    bool embossValue =
-        this.widget.option.emboss == null ? false : this.widget.option.emboss;
-    BorderRadius borderRadiusValue = this.widget.option.borderRadius == null
-        ? BorderRadius.zero
-        : BorderRadius.circular(this.widget.option.borderRadius);
-
-    if (this.widget.option.customBorderRadius != null) {
-      borderRadiusValue = this.widget.option.customBorderRadius;
-    }
-    CurveType curveTypeValue = this.widget.option.curveType == null
-        ? CurveType.none
-        : this.widget.option.curveType;
-
-    List<BoxShadow> shadowList = [
-      BoxShadow(
-          color: _getAdjustColor(
-              parentColorValue, embossValue ? 0 - depthValue : depthValue),
-          offset: Offset(0 - spreadValue, 0 - spreadValue),
-          blurRadius: spreadValue),
-      BoxShadow(
-          color: _getAdjustColor(
-              parentColorValue, embossValue ? depthValue : 0 - depthValue),
-          offset: Offset(spreadValue, spreadValue),
-          blurRadius: spreadValue)
-    ];
-
-    if (embossValue) shadowList = shadowList.reversed.toList();
-    if (embossValue)
-      colorValue = _getAdjustColor(colorValue, 0 - depthValue ~/ 2);
-    if (this.widget.option.surfaceColor != null) colorValue = surfaceColorValue;
-
-    List<Color> gradientColors;
-    switch (curveTypeValue) {
-      case CurveType.concave:
-        gradientColors = _getConcaveGradients(colorValue, depthValue);
-        break;
-      case CurveType.convex:
-        gradientColors = _getConvexGradients(colorValue, depthValue);
-        break;
-      case CurveType.none:
-        gradientColors = _getFlatGradients(colorValue, depthValue);
-        break;
-    }
-
-    return Container(
-      height: heightValue,
-      width: widthValue,
+    Widget _container = Container(
+      height: config.heightValue,
+      width: config.widthValue,
       child: this.widget.option.child,
       decoration: BoxDecoration(
-        borderRadius: borderRadiusValue,
-        color: colorValue,
+        borderRadius: config.borderRadiusValue,
+        color: config.colorValue,
         gradient: LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
-            colors: gradientColors),
-        boxShadow: shadowList,
+            colors: config.gradientColors),
+        boxShadow: config.shadowList,
       ),
     );
+
+    if (this.widget.option.gesture) {
+      _container = GestureDetector(
+        child: _container,
+        onTapDown: (_) {
+          this.widget.option.emboss = true;
+          if (this.widget.option.onTapDown is Function) {
+            this.widget.option.onTapDown();
+          }
+          config.prepareConfig(this.widget.option);
+          setState(() {});
+        },
+        onTapUp: (_) {
+          this.widget.option.emboss = false;
+          if (this.widget.option.onTapDown is Function) {
+            this.widget.option.onTapUp();
+          }
+          config.prepareConfig(this.widget.option);
+          setState(() {});
+        },
+      );
+    }
+
+    return config != null ? _container : Container();
+  }
+}
+
+const Color neumorColor = Color(0xFFf0f0f0);
+
+class NeumorConfig {
+  double heightValue;
+  double widthValue;
+  int depthValue = 20;
+  Color colorValue = neumorColor;
+  Color parentColorValue = neumorColor;
+  Color surfaceColorValue = neumorColor;
+  double spreadValue = 6;
+  bool embossValue = false;
+  BorderRadius borderRadiusValue = BorderRadius.zero;
+  CurveType curveTypeValue = CurveType.none;
+  List<BoxShadow> shadowList;
+  List<Color> gradientColors;
+
+  NeumorConfig({
+    this.heightValue,
+    this.widthValue,
+    this.depthValue,
+    this.colorValue,
+    this.parentColorValue,
+    this.surfaceColorValue,
+    this.spreadValue,
+    this.embossValue,
+    this.borderRadiusValue,
+    this.curveTypeValue,
+  });
+
+  factory NeumorConfig.buildByNeumorOption(NeumorOption _option) {
+    NeumorConfig _config = NeumorConfig();
+
+    _config.prepareProperty(_option);
+    _config.generatorShadowList();
+    _config.setupCustomBorderRadiusValue(_option);
+    _config.detectEmbossValue(_option);
+    _config.generatorGradientColors();
+
+    print(_config);
+
+    return _config;
+  }
+
+  prepareConfig(NeumorOption _option) {
+    this.prepareProperty(_option);
+    this.generatorShadowList();
+    this.setupCustomBorderRadiusValue(_option);
+    this.detectEmbossValue(_option);
+    this.generatorGradientColors();
+  }
+
+  prepareProperty(NeumorOption _option) {
+    this.heightValue = _option.height == null ? null : _option.height;
+    this.widthValue = _option.width == null ? null : _option.width;
+    this.depthValue = _option.depth == null ? 20 : _option.depth;
+    this.colorValue = _option.color == null ? Color(0xFFf0f0f0) : _option.color;
+    this.parentColorValue =
+        _option.parentColor == null ? colorValue : _option.parentColor;
+    this.surfaceColorValue =
+        _option.surfaceColor == null ? colorValue : _option.surfaceColor;
+    this.spreadValue = _option.spread == null ? 6 : _option.spread;
+    this.embossValue = _option.emboss == null ? false : _option.emboss;
+    this.borderRadiusValue = _option.borderRadius == null
+        ? BorderRadius.zero
+        : BorderRadius.circular(_option.borderRadius);
+
+    this.curveTypeValue =
+        _option.curveType == null ? CurveType.none : _option.curveType;
+  }
+
+  generatorShadowList() {
+    this.shadowList = [
+      BoxShadow(
+        color: _getAdjustColor(this.parentColorValue,
+            this.embossValue ? 0 - this.depthValue : this.depthValue),
+        offset: Offset(0 - this.spreadValue, 0 - this.spreadValue),
+        blurRadius: this.spreadValue,
+      ),
+      BoxShadow(
+        color: _getAdjustColor(this.parentColorValue,
+            this.embossValue ? this.depthValue : 0 - this.depthValue),
+        offset: Offset(this.spreadValue, this.spreadValue),
+        blurRadius: this.spreadValue,
+      )
+    ];
+  }
+
+  setupCustomBorderRadiusValue(NeumorOption _option) {
+    if (_option.customBorderRadius != null) {
+      borderRadiusValue = _option.customBorderRadius;
+    }
+  }
+
+  detectEmbossValue(NeumorOption _option) {
+    if (this.embossValue) this.shadowList = this.shadowList.reversed.toList();
+    if (this.embossValue)
+      this.colorValue =
+          _getAdjustColor(this.colorValue, 0 - this.depthValue ~/ 2);
+    if (_option.surfaceColor != null) this.colorValue = this.surfaceColorValue;
+  }
+
+  generatorGradientColors() {
+    switch (this.curveTypeValue) {
+      case CurveType.concave:
+        this.gradientColors =
+            _getConcaveGradients(this.colorValue, this.depthValue);
+        break;
+      case CurveType.convex:
+        this.gradientColors =
+            _getConvexGradients(this.colorValue, this.depthValue);
+        break;
+      default:
+        this.gradientColors =
+            _getFlatGradients(this.colorValue, this.depthValue);
+        break;
+    }
   }
 
   Color _getAdjustColor(Color baseColor, amount) {
@@ -144,21 +196,21 @@ class _NeumorphicContainerState extends State<NeumorphicContainer> {
     return Color.fromRGBO(colors["red"], colors["green"], colors["blue"], 1);
   }
 
-  List<Color> _getFlatGradients(baseColor, depth) {
+  List<Color> _getFlatGradients(Color baseColor, int depth) {
     return [
       baseColor,
       baseColor,
     ];
   }
 
-  List<Color> _getConcaveGradients(baseColor, depth) {
+  List<Color> _getConcaveGradients(Color baseColor, int depth) {
     return [
       _getAdjustColor(baseColor, 0 - depth),
       _getAdjustColor(baseColor, depth),
     ];
   }
 
-  List<Color> _getConvexGradients(baseColor, depth) {
+  List<Color> _getConvexGradients(Color baseColor, int depth) {
     return [
       _getAdjustColor(baseColor, depth),
       _getAdjustColor(baseColor, 0 - depth),
@@ -179,6 +231,9 @@ class NeumorOption {
   CurveType curveType;
   int depth;
   bool emboss;
+  bool gesture;
+  Function onTapUp;
+  Function onTapDown;
 
   NeumorOption({
     this.child,
@@ -193,6 +248,9 @@ class NeumorOption {
     this.curveType,
     this.depth,
     this.emboss,
+    this.gesture,
+    this.onTapUp,
+    this.onTapDown,
   });
 
   factory NeumorOption.build(NeumorOption option) {
@@ -214,6 +272,9 @@ class NeumorOption {
     curveType: ${this.curveType},
     depth: ${this.depth},
     emboss: ${this.emboss},
+    gesture: ${this.gesture},
+    onTapUp: ${this.onTapUp},
+    onTapDown: ${this.onTapDown},
     }''';
   }
 }
--- lib/neumorphic.dart
+++ lib/neumorphic.dart
@@ -6,22 +6,6 @@ class Neumorphic extends StatelessWidget {
   Widget build(BuildContext context) {
     Color baseColor = Color(0xFFF2F2F2);
 
-    NeumorphicContainer neumorphicContainer1 = NeumorphicContainer(NeumorOption(
-      color: baseColor,
-      height: 64,
-      width: 64,
-      borderRadius: 64,
-      emboss: false,
-    ));
-
-    NeumorphicContainer neumorphicContainer2 = NeumorphicContainer(NeumorOption(
-      color: baseColor,
-      height: 64,
-      width: 64,
-      borderRadius: 64,
-      emboss: false,
-    ));
-
     return Container(
       color: baseColor,
       child: Center(
@@ -30,24 +14,31 @@ class Neumorphic extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              GestureDetector(
-                child: neumorphicContainer1,
-                onTapDown: (_) {
-                  neumorphicContainer1.toggleOn();
-                },
-                onTapUp: (_) {
-                  neumorphicContainer1.toggleOff();
-                },
+              NeumorphicContainer(
+                NeumorOption(
+                  color: baseColor,
+                  height: 64,
+                  width: 64,
+                  borderRadius: 64,
+                  emboss: false,
+                  gesture: true,
+                ),
               ),
-              GestureDetector(
-                child: neumorphicContainer2,
-                onTapDown: (_) {
-                  neumorphicContainer2.toggleOn();
-                },
-                onTapUp: (_) {
-                  neumorphicContainer2.toggleOff();
-                },
-              )
+              NeumorphicContainer(
+                NeumorOption(
+                    color: baseColor,
+                    height: 64,
+                    width: 64,
+                    borderRadius: 64,
+                    emboss: false,
+                    gesture: true,
+                    onTapDown: () {
+                      print('onTapDown');
+                    },
+                    onTapUp: () {
+                      print('onTapUp');
+                    }),
+              ),
             ],
           ),
         ),
